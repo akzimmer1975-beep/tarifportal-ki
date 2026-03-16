2746119a9ab432f0506c1d945d6661d14456dc0b
akzimmer1975-beep <akzimmer1975@gmail.com>
2026-03-16 10:30:18 +0100

    jkljhklh

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+
 import "dotenv/config";
+import cors from "cors";
 import express, { type NextFunction, type Request, type Response } from "express";
 import adminRouter from "./routes/admin.js";
 import authRouter from "./routes/auth.js";
@@ -7,6 +9,9 @@ import searchRouter from "./routes/search.js";
 import { chatRouter } from "./routes/chat.js";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
